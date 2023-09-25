324f9a0387da5d8e99a62c75588a801eb5913cfc
probrianz <145468164+probrianz@users.noreply.github.com>
2023-09-25 20:09:31 +0800

    the exercise is done

--- median.py
+++ median.py
@@ -1,6 +1,5 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +9,16 @@ while True:
     else:
         break
 print(numbers)
+
+
+
+total = len(numbers)
+numbers.sort()
+
+if total % 2 == 0:
+	median1 = numbers[total//2]
+	median2 = numbers[total//2 - 1]
+	median = (median1 + median2)/2
+else:
+	median = numbers[total//2]
+print("Median is: " + str(median))
